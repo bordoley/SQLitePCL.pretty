02aa0bb2779ff6caba3f16475403095bcae4a1ae
Dave <bordoley@gmail.com>
2014-11-07 21:47:42 -0800

    Add license headers. Improve test style.

--- SQLitePCL.pretty.tests/SQLiteVersionTests.cs
+++ SQLitePCL.pretty.tests/SQLiteVersionTests.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+   Copyright 2014 David Bordoley
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -70,17 +86,8 @@ namespace SQLitePCL.pretty.tests
             Assert.Less(SQLiteVersion.Of(3080911), SQLiteVersion.Of(3081911));
             Assert.Less(SQLiteVersion.Of(3080911), SQLiteVersion.Of(4080911));
 
-            try
-            {
-                SQLiteVersion.Of(3080911).CompareTo(null);
-            }
-            catch (ArgumentException) {}
-
-            try
-            {
-                SQLiteVersion.Of(3080911).CompareTo("");
-            }
-            catch (ArgumentException) { }
+            Assert.Throws(typeof(ArgumentException), () => SQLiteVersion.Of(3080911).CompareTo(null));
+            Assert.Throws(typeof(ArgumentException), () => SQLiteVersion.Of(3080911).CompareTo(""));
         }
 
         [Test]
