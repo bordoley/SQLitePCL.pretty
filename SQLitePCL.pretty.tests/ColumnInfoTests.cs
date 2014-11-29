c4c307af0456f8e6bab6a73d5815c24739bf8901
Dave <bordoley@gmail.com>
2014-11-29 10:47:43 -0800

    ColumnInfo implements IComparable. Add tests.

---
+++ SQLitePCL.pretty.tests/ColumnInfoTests.cs
@@ -0,0 +1,120 @@
+/*
+   Copyright 2014 David Bordoley
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using NUnit.Framework;
+using System;
+
+namespace SQLitePCL.pretty.tests
+{
+    [TestFixture]
+    public class ColumnInfoTests
+    {
+        [Test]
+        public void TestEquality()
+        {
+            Tuple<string, string, string, string>[] tests =
+                {
+                    Tuple.Create("", "", "", ""),
+                    Tuple.Create("name","", "",""),
+                    Tuple.Create("name","db", "", ""),
+                    Tuple.Create("name","db", "table", ""),
+                    Tuple.Create("name","db", "table", "column"),
+                };
+
+            Assert.False(new ColumnInfo().Equals(null));
+            Assert.False(new ColumnInfo().Equals(new Object()));
+
+            for (int i = 0; i < tests.Length; i++)
+            {
+                for (int j = 0; j < tests.Length; j++)
+                {
+                    var fst = new ColumnInfo(tests[i].Item1, tests[i].Item2, tests[i].Item3, tests[i].Item4);
+                    var snd = new ColumnInfo(tests[j].Item1, tests[j].Item2, tests[j].Item3, tests[j].Item4);
+
+                    if (i == j)
+                    {
+                        Assert.AreEqual(fst, snd);
+                        Assert.True(fst == snd);
+                        Assert.False(fst != snd);
+                    }
+                    else
+                    {
+                        Assert.AreNotEqual(fst, snd);
+                        Assert.False(fst == snd);
+                        Assert.True(fst != snd);
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void TestGetHashcode()
+        {
+            Tuple<string, string, string, string>[] tests =
+                {
+                    Tuple.Create("", "", "", ""),
+                    Tuple.Create("name","", "",""),
+                    Tuple.Create("name","db", "", ""),
+                    Tuple.Create("name","db", "table", ""),
+                    Tuple.Create("name","db", "table", "column"),
+                };
+
+            for (int i = 0; i < tests.Length; i++)
+            {
+                var fst =  new ColumnInfo(tests[i].Item1, tests[i].Item2, tests[i].Item3, tests[i].Item4);
+                var snd = new ColumnInfo(tests[i].Item1, tests[i].Item2, tests[i].Item3, tests[i].Item4);
+
+                Assert.AreEqual(fst.GetHashCode(), snd.GetHashCode());
+            }
+        }
+
+        [Test]
+        public void TestComparison()
+        {
+            ColumnInfo[] tests =
+                {
+                    new ColumnInfo("","", "",""),
+                    new ColumnInfo("name","", "",""),
+                    new ColumnInfo("name","db", "", ""),
+                    new ColumnInfo("name","db", "table", ""),
+                    new ColumnInfo("name","db", "table", "column"),
+                };
+
+            for (int i = 0; i < tests.Length; i++)
+            {
+                for(int j = 0; j < tests.Length; j++)
+                {
+                    if (i < j)
+                    {
+                        Assert.Less(tests[i].CompareTo(tests[j]), 0);
+                        Assert.Less(((IComparable)tests[i]).CompareTo(tests[j]), 0);
+                    }
+                    else if (i == j)
+                    {
+                        Assert.AreEqual(tests[i].CompareTo(tests[j]), 0);
+                        Assert.AreEqual(((IComparable)tests[i]).CompareTo(tests[j]), 0);
+                    }
+                    else
+                    {
+                        Assert.Greater(tests[i].CompareTo(tests[j]), 0);
+                        Assert.Greater(((IComparable)tests[i]).CompareTo(tests[j]), 0);
+                    }
+                }
+            }
+                
+        }
+    }
+}
--- SQLitePCL.pretty/ColumnInfo.cs
+++ SQLitePCL.pretty/ColumnInfo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +9,7 @@ namespace SQLitePCL.pretty
     /// <summary>
     /// Represents information about a single column in <see cref="IStatement"/> result set.
     /// </summary>
-    // FIXME: Implement IComparable
-    public struct ColumnInfo : IEquatable<ColumnInfo> //IComparable<ColumnInfo>, IComparable
+    public struct ColumnInfo : IEquatable<ColumnInfo>, IComparable<ColumnInfo>, IComparable
     {
         /// <summary>
         /// Indicates whether the two ColumnInfo instances are equal to each other.
@@ -45,8 +44,8 @@ namespace SQLitePCL.pretty
 
         private readonly string name;
         private readonly string databaseName;
-        private readonly string originName;
         private readonly string tableName;
+        private readonly string originName;
 
         internal ColumnInfo(string name, string databaseName, string originName, string tableName)
         {
@@ -104,6 +103,34 @@ namespace SQLitePCL.pretty
             hash = hash * 31 + this.OriginName.GetHashCode();
             hash = hash * 31 + this.TableName.GetHashCode();
             return hash;
+        }
+
+        /// <inheritdoc/>
+        public int CompareTo(ColumnInfo other)
+        {
+            if (other != null)
+            {
+                var result = this.Name.CompareTo(other.Name);
+                if (result != 0) { return result; }
+                
+                result = this.DatabaseName.CompareTo(other.DatabaseName);
+                if (result != 0) { return result; }
+
+                result = this.TableName.CompareTo(other.TableName);
+                if (result != 0) { return result; }
+
+                return this.OriginName.CompareTo(other.OriginName);
+            }
+            else 
+            {
+                return 1;
+            }
+        }
+
+        /// <inheritdoc/>
+        int IComparable.CompareTo(object obj)
+        {
+            return this.CompareTo((ColumnInfo)obj);
         }
     }
 }
