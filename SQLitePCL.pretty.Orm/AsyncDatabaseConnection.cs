014f22b7e702034cac51af94e06eef35f06e9c83
Dave <bordoley@gmail.com>
2015-03-09 07:50:30 -0700

    fix namespace

--- SQLitePCL.pretty.Orm/AsyncDatabaseConnection.cs
+++ SQLitePCL.pretty.Orm/AsyncDatabaseConnection.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SQLitePCL.pretty.Orm
+namespace SQLitePCL.pretty
 {
     public static class AsyncDatabaseConnection
     {
