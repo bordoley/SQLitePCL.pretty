25ec16cb799281fb34deb476ca59ec4bd69ab1f7
Dave <bordoley@gmail.com>
2015-03-11 23:26:32 -0700

    add missing files

---
+++ SQLitePCL.pretty/DatabaseConnection.Index.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SQLitePCL.pretty
+{
+    public sealed class IndexedColumn
+    {
+        public enum SortOrder
+        {
+            Ascending,
+            Descending
+        }
+
+        private readonly string name;
+        private readonly string collation;
+        private readonly SortOrder? sortOrder;
+
+        public 
+    }    
+
+    public static partial class DatabaseConnection
+    {   
+        /// <summary>
+        /// Rebuilds all indexes in all attached databases.
+        /// </summary>
+        /// <param name="This">The database connection.</param>
+        /// <seealso href="https://www.sqlite.org/lang_reindex.html"/>
+        public static void ReIndex(this IDatabaseConnection This)
+        {
+            This.Execute(SQLBuilder.ReIndex);
+        }
+
+        /// <summary>
+        /// Delete and recreate indexes from scratch. Useful when the definition of a collation sequence has changed.
+        /// </summary>
+        /// <param name="This">The database connection.</param>
+        /// <param name="name">
+        /// Either a collation sequence name, or a table or index name optionally prefixed by a database name.
+        /// </param>
+        /// <seealso href="https://www.sqlite.org/lang_reindex.html"/>
+        public static void ReIndex(this IDatabaseConnection This, string name)
+        {
+            This.Execute(SQLBuilder.ReIndexWithName(name));
+        }
+    }
+}
+
---
+++ SQLitePCL.pretty/DatabaseConnection.Tables.cs
@@ -0,0 +1,62 @@
+﻿/*
+   Copyright 2014 David Bordoley
+   Copyright 2014 Zumero, LLC
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+
+namespace SQLitePCL.pretty
+{
+    public static partial class DatabaseConnection
+    {
+        /// <summary>
+        /// Renames the table;
+        /// </summary>
+        /// <param name="This">The database connection.</param>
+        /// <param name="table">The table name.</param>
+        /// <param name="newName">The new table name.</param>
+        /// <seealso href="https://www.sqlite.org/lang_altertable.html"/>
+        public static void Rename(this IDatabaseConnection This, string table, string newName)
+        {
+            This.Execute(SQLBuilder.AlterTableRename(table, newName));
+        }
+
+        /// <summary>
+        /// Drops the table.
+        /// </summary>
+        /// <param name="This">The database connection.</param>
+        /// <param name="table">The table name.</param>
+        /// <seealso href="https://www.sqlite.org/lang_droptable.html"/>
+        public static void DropTable(this IDatabaseConnection This, string table)
+        {
+            This.Execute(SQLBuilder.DropTable(table));
+        }
+
+        /// <summary>
+        /// Drops the table if it exists. Otherwise this is a no-op.
+        /// </summary>
+        /// <param name="This">The database connection.</param>
+        /// <param name="table">The table name.</param>
+        /// <seealso href="https://www.sqlite.org/lang_droptable.html"/>
+        public static void DropTableIfExists(this IDatabaseConnection This, string table)
+        {
+            This.Execute(SQLBuilder.DropTableIfExists(table));
+        }
+    }
+}
