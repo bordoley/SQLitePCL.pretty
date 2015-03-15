52f48cf414a180e95b9141935013b853a31a02fe
Dave <bordoley@gmail.com>
2015-03-15 16:12:57 -0700

    Add interface documentation.

--- SQLitePCL.pretty.Orm/Interfaces.cs
+++ SQLitePCL.pretty.Orm/Interfaces.cs
@@ -3,25 +3,50 @@ using System.Collections.Generic;
 
 namespace SQLitePCL.pretty.Orm
 {
+    /// <summary>
+    /// The mapping of a type to a SQL Table.
+    /// </summary>
     public interface ITableMapping : IReadOnlyDictionary<string, ColumnMapping>
     {
+        /// <summary>
+        /// Gets the name of the table.
+        /// </summary>
         String TableName { get; }
 
-        ColumnMapping this[string column] { get; }
-
+        /// <summary>
+        /// Gets the table indexes.
+        /// </summary>
         IEnumerable<IndexInfo> Indexes { get; }
     }
 
+    /// <summary>
+    /// The mapping of a type <see typeparamref="T"/> to a SQL Table.
+    /// </summary>
     public interface ITableMapping<T> : ITableMapping
     {
+        /// <summary>
+        /// Converts a result set row to an instance of type T.
+        /// </summary>
+        /// <returns>An instance of type T.</returns>
+        /// <param name="row">The result set row.</param>
         T ToObject(IReadOnlyList<IResultSetValue> row);
     }
 
+    /// <summary>
+    /// An <see cref="IStatement"/> that allows the current value to be retrieved 
+    /// as an instance T based upon an underlying table mapping.
+    /// </summary>
     public interface ITableMappedStatement<T> : IStatement
     {
+        /// <summary>
+        /// The underlying <see cref="ITableMapping&lt;T&gt;"/> used to map the result set to an instance of T.
+        /// </summary>
         ITableMapping<T> Mapping { get; }
 
-        T Current { get; } 
+        /// <summary>
+        /// Gets the current result value at the current position of the statement enumerator.
+        /// </summary>
+        new T Current { get; } 
     }
 }
 
