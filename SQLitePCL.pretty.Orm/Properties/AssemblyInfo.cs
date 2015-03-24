90123d0bd494ce152e8a612855134b00b243c0ed
Dave <bordoley@gmail.com>
2015-03-24 09:28:20 -0700

    update assembly info

--- SQLitePCL.pretty.Orm/Properties/AssemblyInfo.cs
+++ SQLitePCL.pretty.Orm/Properties/AssemblyInfo.cs
@@ -5,25 +5,9 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("SQLitePCL.pretty.Orm")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("dave")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+[assembly: AssemblyDescription("An ORM for SQLitePCL.pretty")]
+[assembly: AssemblyProduct("SQLitePCL.pretty")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyVersion("0.0.12")]
+[assembly: AssemblyFileVersion("0.0.12")]
 [assembly: InternalsVisibleTo("SQLitePCL.pretty.tests")]
-
