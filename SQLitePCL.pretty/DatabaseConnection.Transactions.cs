69cb3e18c817ab953a640caf1f5c46803d337951
David Bordoley <bordoley@gmail.com>
2015-07-13 01:06:25 -0400

    tabs to spaces.

--- SQLitePCL.pretty/DatabaseConnection.Transactions.cs
+++ SQLitePCL.pretty/DatabaseConnection.Transactions.cs
@@ -33,144 +33,144 @@ using System.IO;
 
 namespace SQLitePCL.pretty
 {
-	internal sealed class TransactionDatabaseConnection : IDatabaseConnection
-	{
-		private readonly IDatabaseConnection db;
+    internal sealed class TransactionDatabaseConnection : IDatabaseConnection
+    {
+        private readonly IDatabaseConnection db;
 
-		private readonly EventHandler rollback;
-		private readonly EventHandler<DatabaseTraceEventArgs> trace;
-		private readonly EventHandler<DatabaseProfileEventArgs> profile;
-		private readonly EventHandler<DatabaseUpdateEventArgs> update;
+        private readonly EventHandler rollback;
+        private readonly EventHandler<DatabaseTraceEventArgs> trace;
+        private readonly EventHandler<DatabaseProfileEventArgs> profile;
+        private readonly EventHandler<DatabaseUpdateEventArgs> update;
 
-		private bool disposed = false;
+        private bool disposed = false;
 
-		internal TransactionDatabaseConnection(IDatabaseConnection db)
-		{
-			this.db = db;
+        internal TransactionDatabaseConnection(IDatabaseConnection db)
+        {
+            this.db = db;
 
-			this.rollback = (o, e) => this.Rollback(this, e);
-			this.trace = (o, e) => this.Trace(this, e);
-			this.profile = (o, e) => this.Profile(this, e);
-			this.update = (o, e) => this.Update(this, e);
+            this.rollback = (o, e) => this.Rollback(this, e);
+            this.trace = (o, e) => this.Trace(this, e);
+            this.profile = (o, e) => this.Profile(this, e);
+            this.update = (o, e) => this.Update(this, e);
 
-			db.Rollback += rollback;
-			db.Trace += trace;
-			db.Profile += profile;
-			db.Update += update;
-		}
+            db.Rollback += rollback;
+            db.Trace += trace;
+            db.Profile += profile;
+            db.Update += update;
+        }
 
-		public event EventHandler Rollback = (o, e) => { };
+        public event EventHandler Rollback = (o, e) => { };
 
-		public event EventHandler<DatabaseTraceEventArgs> Trace = (o, e) => { };
+        public event EventHandler<DatabaseTraceEventArgs> Trace = (o, e) => { };
 
-		public event EventHandler<DatabaseProfileEventArgs> Profile = (o, e) => { };
+        public event EventHandler<DatabaseProfileEventArgs> Profile = (o, e) => { };
 
-		public event EventHandler<DatabaseUpdateEventArgs> Update = (o, e) => { };
+        public event EventHandler<DatabaseUpdateEventArgs> Update = (o, e) => { };
 
-		public bool IsAutoCommit
-		{
-			get
-			{
-				if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
-				return db.IsAutoCommit;
-			}
-		}
+        public bool IsAutoCommit
+        {
+            get
+            {
+                if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
+                return db.IsAutoCommit;
+            }
+        }
 
-		public bool IsReadOnly
-		{
-			get
-			{
-				if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
-				return db.IsReadOnly;
-			}
-		}
+        public bool IsReadOnly
+        {
+            get
+            {
+                if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
+                return db.IsReadOnly;
+            }
+        }
 
-		public int Changes
-		{
-			get
-			{
-				if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
-				return db.Changes;
-			}
-		}
+        public int Changes
+        {
+            get
+            {
+                if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
+                return db.Changes;
+            }
+        }
 
-		public int TotalChanges
-		{
-			get
-			{
-				if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
-				return db.TotalChanges;
-			}
-		}
+        public int TotalChanges
+        {
+            get
+            {
+                if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
+                return db.TotalChanges;
+            }
+        }
 
-		public long LastInsertedRowId
-		{
-			get
-			{
-				if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
-				return db.LastInsertedRowId;
-			}
-		}
+        public long LastInsertedRowId
+        {
+            get
+            {
+                if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
+                return db.LastInsertedRowId;
+            }
+        }
 
-		public IEnumerable<IStatement> Statements
-		{
-			get
-			{
-				if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
-				return db.Statements;
-			}
-		}
+        public IEnumerable<IStatement> Statements
+        {
+            get
+            {
+                if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
+                return db.Statements;
+            }
+        }
 
-		public void WalCheckPoint(string dbName, WalCheckPointMode mode, out int nLog, out int nCkpt)
-		{
-			if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
-			db.WalCheckPoint(dbName, mode, out nLog, out nCkpt);
-		}
+        public void WalCheckPoint(string dbName, WalCheckPointMode mode, out int nLog, out int nCkpt)
+        {
+            if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
+            db.WalCheckPoint(dbName, mode, out nLog, out nCkpt);
+        }
 
-		public bool IsDatabaseReadOnly(string dbName)
-		{
-			if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
-			return db.IsDatabaseReadOnly(dbName);
-		}
+        public bool IsDatabaseReadOnly(string dbName)
+        {
+            if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
+            return db.IsDatabaseReadOnly(dbName);
+        }
 
-		public TableColumnMetadata GetTableColumnMetadata(string dbName, string tableName, string columnName)
-		{
-			if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
-			return db.GetTableColumnMetadata(dbName, tableName, columnName);
-		}
+        public TableColumnMetadata GetTableColumnMetadata(string dbName, string tableName, string columnName)
+        {
+            if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
+            return db.GetTableColumnMetadata(dbName, tableName, columnName);
+        }
 
-		public Stream OpenBlob(string database, string tableName, string columnName, long rowId, bool canWrite)
-		{
-			if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
-			return db.OpenBlob(database, tableName, columnName, rowId, canWrite);
-		}
+        public Stream OpenBlob(string database, string tableName, string columnName, long rowId, bool canWrite)
+        {
+            if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
+            return db.OpenBlob(database, tableName, columnName, rowId, canWrite);
+        }
 
-		public IStatement PrepareStatement(string sql, out string tail)
-		{
-			if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
+        public IStatement PrepareStatement(string sql, out string tail)
+        {
+            if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
 
-			return db.PrepareStatement(sql, out tail);
-		}
+            return db.PrepareStatement(sql, out tail);
+        }
 
-		public void Status(DatabaseConnectionStatusCode statusCode, out int current, out int highwater, bool reset)
-		{
-			if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
-			this.db.Status(statusCode, out current, out highwater, reset);
-		}
+        public void Status(DatabaseConnectionStatusCode statusCode, out int current, out int highwater, bool reset)
+        {
+            if (disposed) { throw new ObjectDisposedException(this.GetType().FullName); }
+            this.db.Status(statusCode, out current, out highwater, reset);
+        }
 
-		public void Dispose()
-		{
-			db.Rollback -= rollback;
-			db.Trace -= trace;
-			db.Profile -= profile;
-			db.Update -= update;
+        public void Dispose()
+        {
+            db.Rollback -= rollback;
+            db.Trace -= trace;
+            db.Profile -= profile;
+            db.Update -= update;
 
-			// Guard against someone taking a reference to this and trying to use it outside of
-			// the Use function delegate
-			disposed = true;
-			// We don't actually own the database connection so its not disposed
-		}
-	}
+            // Guard against someone taking a reference to this and trying to use it outside of
+            // the Use function delegate
+            disposed = true;
+            // We don't actually own the database connection so its not disposed
+        }
+    }
 
     /// <summary>
     /// Database transaction modes.
@@ -201,34 +201,34 @@ namespace SQLitePCL.pretty
     {
         private static readonly ThreadLocal<Random> _rand = new ThreadLocal<Random>(() => new Random());
 
-		private static void BeginTransaction(this IDatabaseConnection This, TransactionMode mode)
+        private static void BeginTransaction(this IDatabaseConnection This, TransactionMode mode)
         {
             var beginTransactionString = SQLBuilder.BeginTransactionWithMode(mode);
             This.Execute(beginTransactionString);
         }
-			
+            
         private static string SaveTransactionPoint(this IDatabaseConnection This)
         {
             var savePoint = "S" + _rand.Value.Next (short.MaxValue);
             This.Execute(SQLBuilder.SavePoint(savePoint));
             return savePoint;
         }
-			
+            
         private static void ReleaseTransaction(this IDatabaseConnection This, string savepoint)
         {
             This.Execute(SQLBuilder.Release(savepoint));
         }
-			
+            
         private static void CommitTransaction(this IDatabaseConnection This)
         {
             This.Execute(SQLBuilder.CommitTransaction);
         }
-			
+            
         private static void RollbackTransactionTo(this IDatabaseConnection This, string savepoint)
         {
             This.Execute(SQLBuilder.RollbackTransactionTo(savepoint));
         }
-			
+            
         private static void RollbackTransaction(this IDatabaseConnection This)
         {
             This.Execute(SQLBuilder.RollbackTransaction);
@@ -295,44 +295,44 @@ namespace SQLitePCL.pretty
             Contract.Requires(This != null);
             Contract.Requires(f != null);
 
-			using(var db = new TransactionDatabaseConnection(This))
-			{
-				string savePoint = null;
+            using(var db = new TransactionDatabaseConnection(This))
+            {
+                string savePoint = null;
 
-				if (This is TransactionDatabaseConnection) 
-				{
-					savePoint = db.SaveTransactionPoint();
-				}
+                if (This is TransactionDatabaseConnection) 
+                {
+                    savePoint = db.SaveTransactionPoint();
+                }
 
-				try
-				{
-					var retval = f(db);
+                try
+                {
+                    var retval = f(db);
 
-					if (savePoint != null)
-					{
-						db.ReleaseTransaction(savePoint);
-					}
-					else
-					{
-						db.CommitTransaction(); 
-					} 
+                    if (savePoint != null)
+                    {
+                        db.ReleaseTransaction(savePoint);
+                    }
+                    else
+                    {
+                        db.CommitTransaction(); 
+                    } 
 
-					return retval;
-				}
-				catch (Exception)
-				{
-					if (savePoint != null) 
-					{
-						db.RollbackTransactionTo(savePoint);
-					}
-					else
-					{
-						db.RollbackTransaction();
-					}
+                    return retval;
+                }
+                catch (Exception)
+                {
+                    if (savePoint != null) 
+                    {
+                        db.RollbackTransactionTo(savePoint);
+                    }
+                    else
+                    {
+                        db.RollbackTransaction();
+                    }
 
-					throw;
-				}
-			}
+                    throw;
+                }
+            }
         }
 
         /// <summary>
